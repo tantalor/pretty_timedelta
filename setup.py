9ea84ee52b8d29454b461034992ad24c9d285cd2
John Tantalo <john.tantalo@gmail.com>
2011-08-20 12:53:44 -0700

    setup script

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+  name='pretty_timedelta',
+  description='Formats timedelta objects as pretty text in any language.',
+  author='John Tantalo',
+  author_email='john.tantalo@gmail.com',
+  url='http://github.com/tantalor/pretty_timedelta',
+  packages=['pretty_timedelta'],
+)
